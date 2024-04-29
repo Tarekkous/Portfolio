71efde6893fdf5456598a3b7ad6517e893fdc32a
Tarekkous <tarek.koussaier@yahoo.fr>
2024-04-29 16:55:55 +0200

    init project

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import './App.scss';
+
+function App() {
+
+
+const Layout= () => {
+  <>
+  
+  </>
+}
+
+
+
+  return (
+    <div>
+      <RouterProvider router={router}/>
+    </div>
+  );
+}
+
+export default App;
