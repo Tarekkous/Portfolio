e4aaec7b69c092c5fbd17dfccae51405b002211d
Tarekkous <tarek.koussaier@yahoo.fr>
2024-05-02 16:04:45 +0200

    Building contact form

--- client/src/pages/contact/Contact.jsx
+++ client/src/pages/contact/Contact.jsx
@@ -1,13 +1,52 @@
-import React from 'react'
-import "../contact/contact.scss"
-
+import React, { useRef } from "react";
+import "../contact/contact.scss";
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
-  return (
-    <div className='contact'>
-      Contact
-    </div>
-  )
+
+const refForm = useRef()
+
+const sendEmail = (e) => {
+e.preventDefault()
 }
 
-export default Contact
+  return (
+    <div className="container">
+      <div className="contact">
+        <h1>CONTACT ME</h1>
+        <div className="contact-form">
+          <form ref={refForm} onSubmit={sendEmail}>
+            <ul>
+              <li className="half">
+                <input type="text" name="name" placeholder="Name" required />
+              </li>
+              <li className="half">
+                <input type="email" name="email" placeholder="Email" required />
+              </li>
+              <li className="half">
+                <input
+                  type="text"
+                  name="subject"
+                  placeholder="Subject"
+                  required
+                />
+              </li>
+              <li className="half">
+                <textarea
+                  name="message"
+                  placeholder="Message"
+                  required
+                ></textarea>
+              </li>
+              <li>
+                <input type="submit" className="flat-button" value={"SEND"} />
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
